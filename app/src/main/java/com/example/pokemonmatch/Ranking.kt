2321121ba875e203c114ce951087b1bba0333918
Jieco Lao <jiecoandres.lao07@gmail.com>
2025-04-17 06:56:54 +0800

    done loginsignup

--- app/src/main/java/com/example/pokemonmatch/Ranking.kt
+++ app/src/main/java/com/example/pokemonmatch/Ranking.kt
@@ -6,17 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainMenu : AppCompatActivity() {
+class Ranking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Login_Signup)) { v, insets ->
+        setContentView(R.layout.activity_ranking)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
-
 }
