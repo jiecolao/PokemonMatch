bc8659e333897545ecc2251fc23fa9d2ad2755e7
Jieco Lao <jiecoandres.lao07@gmail.com>
2025-04-17 18:17:37 +0800

    frontend done

--- app/src/main/java/com/example/pokemonmatch/MainMenu.kt
+++ app/src/main/java/com/example/pokemonmatch/MainMenu.kt
@@ -10,8 +10,8 @@ class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_hgame)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.hgame)) { v, insets ->
+        setContentView(R.layout.activity_loginsignup)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Login_Signup)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
