d294f64fabdc57434415a7c347d22e3203c7043a
Jieco Lao <jiecoandres.lao07@gmail.com>
2025-04-17 10:23:12 +0800

    done selectdifficulty

--- app/src/main/java/com/example/pokemonmatch/CardCatalogue.kt
+++ app/src/main/java/com/example/pokemonmatch/CardCatalogue.kt
@@ -6,17 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainMenu : AppCompatActivity() {
+class CardCatalogue : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_selectdifficulty)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.selectdiff)) { v, insets ->
+        setContentView(R.layout.activity_cardcatalogue)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cardcat)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
-
 }
