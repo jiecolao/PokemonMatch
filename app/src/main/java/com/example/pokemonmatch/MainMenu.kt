254226255aa8ecad3148eb899a009df91a281571
Jieco Lao <jiecoandres.lao07@gmail.com>
2025-04-17 08:22:54 +0800

    leaderboards done

--- app/src/main/java/com/example/pokemonmatch/MainMenu.kt
+++ app/src/main/java/com/example/pokemonmatch/MainMenu.kt
@@ -10,8 +10,8 @@ class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Login_Signup)) { v, insets ->
+        setContentView(R.layout.activity_ranking)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ranking)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/pokemonmatch/Options.kt
+++ app/src/main/java/com/example/pokemonmatch/Options.kt
@@ -10,7 +10,7 @@ class Options : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.options)
+        setContentView(R.layout.activity_options)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
